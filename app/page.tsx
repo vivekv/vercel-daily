36c955ff456ed25ac4c5da6f39be31ed2cb6065d
Vivek Varadarajan <vivek.varadarajan@slalom.com>
2026-04-11 21:25:03 -0400

    adding the article id to the URL

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         description="You can just use AI. Any vendor, any model. The Vercel AI Gateway is now available for alpha testing.Built on the AI SDK 5 alpha, the Gateway lets you switch between ~100 AI models without needing to manage API keys, rate limits, or provider accounts. The Gateway handles authentication, usage tracking, and in the future, billing."
         imageSrc="https://i8qy5y6gxkdgdcv9.public.blob.vercel-storage.com/news/introducing-the-ai-gateway.png"
         imageAlt="Featured article"
-        mainCta="/articles/introducing-the-ai-gateway"
+        mainCta="/articles/introducing-the-ai-gateway/7kLbZipHbSIgopHtLzI29K"
       />
       <Suspense>
         <FeaturedArticles />
